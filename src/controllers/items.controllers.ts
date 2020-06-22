a64c6cf8f6790e7c3040415e7f7fb90940dffe33
Pablo Nunes <pablo.machado@ipsm.gov.br>
2020-06-21 22:19:45 -0300

    alterações no controller

--- src/controllers/items.controllers.ts
+++ src/controllers/items.controllers.ts
@@ -6,6 +6,7 @@ class ItemsController {
     const items = await Knex('items').select('*')
     const mappingItems = items.map(item => {
       return {
+        id: item.id,
         title: item.title,
         image_url: `http://localhost:3333/uploads/${item.image}`
       }
